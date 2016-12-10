b87f2ab421d5acb122e7f57116cb9b94ddc02701
Korrigan <rainrider37@gmail.com>
2016-12-11 00:29:57 +0100

    Add mother function to prevent redundant code

---
+++ real_estate_crawling/location/spiders/offer_spider.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from scrapy.http.request import Request
+import scrapy
+
+class offerSpider(scrapy.Spider):
+    def __init__(self, max_price=None, min_price=None, max_area=None, min_area=None):
+        self.max_price = max_price if max_price else ''
+        self.min_price = min_price if min_price else ''
+        self.max_area = max_area if max_area else ''
+        self.min_area = min_area if min_area else ''
+
+    def parse(self, response):
+        yield Request(response.url, self.parse_next_page, dont_filter=True)
+
+    def parse_next_page(self, response):
+        pass
+
+    def parse_one_annonce(self, response):
+        pass
