a575373ace6b0dafd43bbfef7c8dc73d5b84e28c
korriliam <rainrider37@gmail.com>
2017-01-04 00:06:14 +0000

    Changed var name in stats retrieval class

--- real_estate_crawling/util/statsToDb.py
+++ real_estate_crawling/util/statsToDb.py
@@ -9,8 +9,9 @@ from scrapy import signals
 from django.core.exceptions import MultipleObjectsReturned
 from datetime import datetime
 import time
+import scrapy
 
-class statsToDb(object):
+class statsToDb(scrapy.statscollectors.MemoryStatsCollector):
     """
     Sauvegarde de statistiques en base de données à la fermeture du programme.
     """
@@ -31,29 +32,29 @@ class statsToDb(object):
         offset = datetime.fromtimestamp(epoch) - datetime.utcfromtimestamp(epoch)
         return utc + offset
 
-    def stats_spider_closed(self, spider, spider_stats):
+    def stats_spider_closed(self, spider):
         """
         this function is called whenever the spider is done with its work
         """
         item = Statistic()
-        # on recupere certaines statistiques qui sont contenues dans un tableau nommé spider_stats.
-        item.startTime  = unicode(self.utc2local(spider_stats['start_time']).replace(microsecond=0))
-        item.finishTime = unicode(self.utc2local(spider_stats['finish_time']).replace(microsecond=0))
-        item.finishReason = spider_stats['finish_reason'].encode('utf-8') if 'finish_reason' in spider_stats else 0
-        item.requestBytes = spider_stats['downloader/request_bytes'] if 'downloader/request_bytes' in spider_stats else 0
-        item.responseBytes = spider_stats['downloader/response_bytes'] if 'downloader/response_bytes' in spider_stats else 0
-        item.requestCount = spider_stats['downloader/request_count'] if 'downloader/request_count' in spider_stats else 0
-        item.responseCount = spider_stats['downloader/response_count'] if 'downloader/response_count' in spider_stats else 0
-        item.responseReceivedCount = spider_stats['response_received_count'] if 'response_received_count' in spider_stats else 0
-        item.requestDepthMax = spider_stats['request_depth_max'] if 'request_depth_max' in spider_stats else 0
-        item.requestStatusCount200 = spider_stats['downloader/response_status_count/200'] if 'downloader/response_status_count/200' in spider_stats else 0
-        item.requestStatusCount301 = spider_stats['downloader/response_status_count/301'] if 'downloader/response_status_count/301' in spider_stats else 0
-        item.requestStatusCount302 = spider_stats['downloader/response_status_count/302'] if 'downloader/response_status_count/302' in spider_stats else 0
-        item.requestStatusCount500 = spider_stats['downloader/response_status_count/500'] if 'downloader/response_status_count/500' in spider_stats else 0
-        item.requestStatusCount404 = spider_stats['downloader/response_status_count/404'] if 'downloader/response_status_count/404' in spider_stats else 0
-        item.dupeFiltered = spider_stats['request_depth_max'] if 'request_depth_max' in spider_stats else 0
-        item.imgCount = spider_stats['images_count'] if 'images_count' in spider_stats else 0
-        item.nbScrapedItems = spider_stats['item_scraped_count'] if 'item_scraped_count' in spider_stats else 0
+        # on recupere certaines statistiques qui sont contenues dans un tableau nommé self.spider_stats.
+        item.startTime  = unicode(self.utc2local(self.spider_stats['start_time']).replace(microsecond=0))
+        item.finishTime = unicode(self.utc2local(self.spider_stats['finish_time']).replace(microsecond=0))
+        item.finishReason = self.spider_stats['finish_reason'].encode('utf-8') if 'finish_reason' in self.spider_stats else 0
+        item.requestBytes = self.spider_stats['downloader/request_bytes'] if 'downloader/request_bytes' in self.spider_stats else 0
+        item.responseBytes = self.spider_stats['downloader/response_bytes'] if 'downloader/response_bytes' in self.spider_stats else 0
+        item.requestCount = self.spider_stats['downloader/request_count'] if 'downloader/request_count' in self.spider_stats else 0
+        item.responseCount = self.spider_stats['downloader/response_count'] if 'downloader/response_count' in self.spider_stats else 0
+        item.responseReceivedCount = self.spider_stats['response_received_count'] if 'response_received_count' in self.spider_stats else 0
+        item.requestDepthMax = self.spider_stats['request_depth_max'] if 'request_depth_max' in self.spider_stats else 0
+        item.requestStatusCount200 = self.spider_stats['downloader/response_status_count/200'] if 'downloader/response_status_count/200' in self.spider_stats else 0
+        item.requestStatusCount301 = self.spider_stats['downloader/response_status_count/301'] if 'downloader/response_status_count/301' in self.spider_stats else 0
+        item.requestStatusCount302 = self.spider_stats['downloader/response_status_count/302'] if 'downloader/response_status_count/302' in self.spider_stats else 0
+        item.requestStatusCount500 = self.spider_stats['downloader/response_status_count/500'] if 'downloader/response_status_count/500' in self.spider_stats else 0
+        item.requestStatusCount404 = self.spider_stats['downloader/response_status_count/404'] if 'downloader/response_status_count/404' in self.spider_stats else 0
+        item.dupeFiltered = self.spider_stats['request_depth_max'] if 'request_depth_max' in self.spider_stats else 0
+        item.imgCount = self.spider_stats['images_count'] if 'images_count' in self.spider_stats else 0
+        item.nbScrapedItems = self.spider_stats['item_scraped_count'] if 'item_scraped_count' in self.spider_stats else 0
         # try:
         #     item.Crawler = Crawler.objects.get(nameCrawler=spider.name)
         # except MultipleObjectsReturned:
