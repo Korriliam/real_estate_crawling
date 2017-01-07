afdda197a593bd6f49a88cf0272f0de8b7ab0f48
Korrigan <rainrider37@gmail.com>
2017-01-07 02:05:43 +0100

    Tackling specific case such as pap & seloger in check_offer spider

--- real_estate_crawling/location/spiders/check_offer.py
+++ real_estate_crawling/location/spiders/check_offer.py
@@ -5,12 +5,13 @@ from scrapy.spiders import CrawlSpider
 from util.database import check_connection
 from MySQLdb import OperationalError
 import logging
+import re
 
 log = logging.getLogger(__name__)
 
 class CheckOffer(CrawlSpider):
     name = "check_offer"
-    handle_httpstatus_list = [404, 500]
+    handle_httpstatus_list = [200, 301, 404, 500]
 
     def __init__(self):
         super(self.__class__, self).__init__()
@@ -26,10 +27,19 @@ class CheckOffer(CrawlSpider):
     def parse(self, response):
         check_connection()
         if response.status in (404, 500):
-            log.info('Toggling offer to disabled')
-            log.info('url %s unfound' % response.request.meta)
-            obj = Offer.objects.filter(url=response.request.meta['redirect_urls'][0])[0] if 'redirect_urls' in response.request.meta else \
-                Offer.objects.filter(url=response.url)[0]
-            obj.active = False
-            obj.save()
+            self.toggle_disable(response)
+        elif 'redirect_urls' in response.request.meta:
+            if not re.match('.*-g439-\d{2}-r\d+$', response.url) and 'pap.fr' in response.url:
+                log.info('PAPTOGG')
+                self.toggle_disable(response)
+            if not re.match('.*\d+\.htm\\?.*', response.url):
+                log.info('SELOGERTOGG')
+                self.toggle_disable(response)
 
+    def toggle_disable(self, response):
+        log.info('Toggling offer to disabled')
+        log.info('url %s unfound' % response.request.meta)
+        obj = Offer.objects.filter(url=response.request.meta['redirect_urls'][0])[0] if 'redirect_urls' in response.request.meta else \
+            Offer.objects.filter(url=response.url)[0]
+        obj.active = False
+        obj.save()
